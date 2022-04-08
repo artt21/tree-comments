559d106be83ef0be5638b29e4759887d83e45023
Artur <artur.novikevich@gmail.com>
2022-04-08 13:10:47 -0400

    Apr 08, 2022

--- comments.php
+++ comments.php
@@ -9,11 +9,13 @@ function getComments(): array
 
     $pdo = new PDO("mysql:host=localhost;dbname=comments;charset=utf8", "root", "");
 
-    $sql = "SELECT * FROM comments ORDER BY parent_id ASC, `date` ASC";
-    $statement = $pdo->prepare($sql);
-    $statement->execute();
-    $comments = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+    $sql = "SELECT * FROM comments ORDER BY parent_id ASC, `date` ASC";
+    $statement = $pdo->query($sql);
+    $comments = [];
+    while ($comment = $statement->fetch(PDO::FETCH_ASSOC)){
+        $comments[$comment['id']] = $comment;
+    }
     return $comments;
 
 }
@@ -28,13 +30,12 @@ function sortComments(array $comments): array
 
     $arr = [];
 
-    foreach ($comments as $comment){
-        if($comment['parent_id'] === null){
-            $arr[$comment['id']] = $comment;
-        } else{
-            $arr[$comment['parent_id']]['children'][] = $comment;
+    foreach ($comments as $id => &$comment) {
+        if ($comment['parent_id'] === null){
+            $arr[$id] = &$comment;
+        }else{
+            $comments[$comment['parent_id']]['children'][$id] = &$comment;
         }
-
     }
 
     return $arr;
@@ -65,29 +66,34 @@ function addComment(string $name, string $content, ?int $parentId = null): void
  * Validation of records put into the form
  * @return array
  */
-function dataValidation():array
+function dataValidation(): array
 {
 
     $errorMessage = [];
 
     $name = trim($_POST['name']);
     $content = trim($_POST['content']);
-    $allowedSymbols = "/^[a-zA-Z-' ]*$/";
+    $allowedSymbols = "/^[a-zA-Z0-9-',.:!? ]*$/";
     $nameLength = 50;
     $contentLength = 1000;
 
     if (mb_strlen($name) > $nameLength) {
         $errorMessage['name'] = "Name should have less than 30 symbols";
-    } if (mb_strlen($content) > $contentLength) {
+    }
+    if (mb_strlen($content) > $contentLength) {
         $errorMessage['content'] = "Commentary should have less than 1000 symbols";
-    } if (empty($name)) {
+    }
+    if (empty($name)) {
         $errorMessage['name'] = "Enter your name";
-    } if (empty($content)) {
+    }
+    if (empty($content)) {
         $errorMessage['content'] = "Enter your commentary";
-    } if (!preg_match($allowedSymbols, $name)) {
-        $errorMessage['name'] = "Only letters and white space allowed";
-    } if (!preg_match($allowedSymbols, $content)) {
-        $errorMessage['content'] = "Only letters and white space allowed";
+    }
+    if (!preg_match($allowedSymbols, $name)) {
+        $errorMessage['name'] = "Such symbols are not allowed";
+    }
+    if (!preg_match($allowedSymbols, $content)) {
+        $errorMessage['content'] = "Such symbols are not allowed";
     }
 
     return $errorMessage;
